a1b1ab0ae51e30932b9d65100b2c3f2d826edd82
kartikp36 <kartikp2002@gmail.com>
2020-11-24 23:48:00 +0530

    fix: Formatting

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -3,15 +3,11 @@ import React from "react";
 import { Keypad } from "./components/Keypad";
 
 function App() {
-<<<<<<< Updated upstream
-	return (
-		<center>
-			<Keypad />
-		</center>
-	);
-=======
-  return <Keypad />;
->>>>>>> Stashed changes
+  return (
+    <center>
+      <Keypad />
+    </center>
+  );
 }
 
 export default App;
--- calculator/src/components/Keypad.js
+++ calculator/src/components/Keypad.js
@@ -2,60 +2,48 @@ import React from "react";
 import { useState } from "react";
 
 export const Keypad = () => {
-	const [result, setResult] = useState("");
-	const calculate = () => {
-		setResult(eval(result));
-	};
+  const [result, setResult] = useState("");
+  const calculate = () => {
+    setResult(eval(result));
+  };
 
-	const renderKeys = () => {
-		const array = [];
-		for (var i = 0; i < 10; i++) {
-			array.push(
-				<button name={String(i)} onClick={buttonPressed}>
-					{i}
-				</button>
-			);
-		}
+  const renderKeys = () => {
+    const array = [];
+    for (var i = 0; i < 10; i++) {
+      array.push(
+        <button name={String(i)} onClick={buttonPressed}>
+          {i}
+        </button>
+      );
+    }
 
-		const operations = ["+", "-", "*", "/", "=", "Clear All"];
-		array.push(
-			operations.map((i) => (
-				<button name={String(i)} onClick={buttonPressed}>
-					{i}
-				</button>
-			))
-		);
-		return array;
-	};
+    const operations = ["+", "-", "*", "/", "=", "Clear All"];
+    array.push(
+      operations.map((i) => (
+        <button name={String(i)} onClick={buttonPressed}>
+          {i}
+        </button>
+      ))
+    );
+    return array;
+  };
 
-<<<<<<< Updated upstream
-	const buttonPressed = (button) => {
-		if (button.target.name === "=") {
-			calculate();
-		} else if (button.target.name === "Clear All") {
-			setResult("");
-		} else {
-			setResult(result + button.target.name);
-		}
-	};
+  const buttonPressed = (button) => {
+    if (button.target.name === "=") {
+      calculate();
+    } else if (button.target.name === "Clear All") {
+      setResult("");
+    } else {
+      setResult(result + button.target.name);
+    }
+  };
 
-	return (
-		<div>
-			<h2>Calculator</h2>
-
-			<h3>{result}</h3>
-			{renderKeys()}
-		</div>
-	);
-=======
   return (
-    <div className='display'>
-      <div>
-        <p>Calculator</p>
-        {result}
-      </div>
-      <div className='buttons'>{renderKeys()}</div>
+    <div>
+      <h2>Calculator</h2>
+
+      <h3>{result}</h3>
+      {renderKeys()}
     </div>
   );
->>>>>>> Stashed changes
 };
