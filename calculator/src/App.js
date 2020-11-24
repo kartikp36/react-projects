9398d6e31ac64952dac5f3c3e78f65ca12f4db2c
kartikp36 <kartikp2002@gmail.com>
2020-11-24 22:05:03 +0530

    fix: generate buttons by loop
    feat: Arranged buttons

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -3,11 +3,15 @@ import React from "react";
 import { Keypad } from "./components/Keypad";
 
 function App() {
+<<<<<<< Updated upstream
 	return (
 		<center>
 			<Keypad />
 		</center>
 	);
+=======
+  return <Keypad />;
+>>>>>>> Stashed changes
 }
 
 export default App;
