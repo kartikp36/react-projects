6ad8d947818b317ae223a6d105361e5c5145757f
kartikp36 <kartikp2002@gmail.com>
2020-11-27 11:35:52 +0530

    fix: Change class components to function

--- to-do-list/src/components/ToggleTodo.js
+++ to-do-list/src/components/ToggleTodo.js
@@ -1,15 +1,19 @@
 import React from "react";
 
-export default (props) => (
-  <div
-    style={{
-      textDecoration: props.todo.complete ? "line-through" : "",
-    }}>
-    <input
-      type='checkbox'
-      checked={props.todo.complete}
-      onChange={props.toggleComplete}></input>
+export const ToggleTodo = (props) => {
+  return (
+    <div
+      style={{
+        textDecoration: props.todo.complete ? "line-through" : "",
+      }}>
+      <input
+        type='checkbox'
+        checked={props.todo.complete}
+        onChange={() => {
+          props.toggleComplete(props.todo.id);
+        }}></input>
 
-    {props.todo.text}
-  </div>
-);
+      {props.todo.text}
+    </div>
+  );
+};
